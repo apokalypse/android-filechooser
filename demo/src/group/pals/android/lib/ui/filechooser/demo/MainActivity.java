22485caf2fe60c7331eb4a5bfac09e84e7cc49f2
hai bison <none@none>
2012-08-28 19:46:19 +0700

    Update demo.

--- demo/src/group/pals/android/lib/ui/filechooser/demo/MainActivity.java
+++ demo/src/group/pals/android/lib/ui/filechooser/demo/MainActivity.java
@@ -17,14 +17,28 @@
 package group.pals.android.lib.ui.filechooser.demo;
 
 import group.pals.android.lib.ui.filechooser.FileChooserActivity;
+import group.pals.android.lib.ui.filechooser.io.LocalFile;
 import group.pals.android.lib.ui.filechooser.services.IFileProvider;
+import group.pals.android.lib.ui.filechooser.utils.ui.Dlg;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 /**
  * Main activity of this demo.
@@ -67,6 +81,38 @@ public class MainActivity extends Activity {
         mBtnCreateHugeDir.setOnClickListener(mBtnCreateHugeDirOnClickListener);
     }// onCreate()
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case _ReqChooseFile:
+            if (resultCode == RESULT_OK) {
+                /*
+                 * a list of files will always return, if selection mode is
+                 * single, the list contains one file
+                 */
+                List<LocalFile> files = (List<LocalFile>) data.getSerializableExtra(FileChooserActivity._Results);
+                StringBuffer msg = new StringBuffer("You chose:\n");
+                for (File f : files) {
+                    if (f.isDirectory())
+                        msg.append(String.format(" — [Dir] %s\n", f.getName()));
+                    else
+                        msg.append(String.format(" — %s\n", f.getName()));
+                }
+
+                Dlg.showInfo(MainActivity.this, msg);
+            }
+            break;// _ReqChooseFile
+
+        case _ReqSaveAs:
+            if (resultCode == RESULT_OK) {
+                List<LocalFile> files = (List<LocalFile>) data.getSerializableExtra(FileChooserActivity._Results);
+                Dlg.showInfo(MainActivity.this,
+                        String.format("Data will be saved to \"%s\"", files.get(0).getAbsolutePath()));
+            }
+            break;// _ReqSaveAs
+        }
+    }// onActivityResult()
+
     // =========
     // LISTENERS
 
@@ -106,9 +152,116 @@ public class MainActivity extends Activity {
 
     private final View.OnClickListener mBtnCreateHugeDirOnClickListener = new View.OnClickListener() {
 
+        private static final int _MsgDone = 0;
+        private static final int _MsgProgress = 1;
+
+        private Handler mHandler;
+        private Thread mThread;
+        private ProgressDialog mProgressDlg;
+
         @Override
         public void onClick(View v) {
-            //
+            final File _dir = createOrRetrieveExternalTempDir(MainActivity.this);
+
+            if (_dir == null || !_dir.isDirectory()) {
+                Toast.makeText(MainActivity.this, "Can't access external SD card", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mHandler = new Handler() {
+
+                @Override
+                public void handleMessage(Message msg) {
+                    switch (msg.what) {
+                    case _MsgProgress:
+                        if (mProgressDlg != null) {
+                            mProgressDlg.setMessage(String.format("Loading… %.02f%% done", msg.obj));
+                        }
+                        break;// _MsgProgress
+
+                    case _MsgDone:
+                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                        break;// _MsgDone
+                    }
+                }
+            };// mHandler
+
+            mProgressDlg = ProgressDialog.show(MainActivity.this, null, "Processing…", true, true,
+                    new DialogInterface.OnCancelListener() {
+
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            if (mThread != null)
+                                mThread.interrupt();
+                        }
+                    });// ProgressDialog
+            mProgressDlg.setCanceledOnTouchOutside(true);
+
+            mThread = new Thread() {
+
+                private static final int _Count = (int) 1e3;
+
+                @Override
+                public void run() {
+                    int step = 5;
+
+                    final String _dirPath = _dir.getAbsolutePath();
+
+                    for (int i = 0; i < _Count; i++) {
+                        if (isInterrupted())
+                            break;
+
+                        try {
+                            File file = new File(String.format("%s/%,09d", _dirPath, i));
+                            if (!file.exists())
+                                file.createNewFile();
+                        } catch (IOException e) {
+                            interrupt();
+                        }
+
+                        if ((i + 1.0) / _Count * 100 >= step) {
+                            Message msg = new Message();
+                            msg.what = _MsgProgress;
+                            msg.obj = (i + 1.0) / _Count * 100;
+                            mHandler.sendMessage(msg);
+
+                            step += 5;
+                        }
+                    }// for i
+
+                    mHandler.sendEmptyMessage(_MsgDone);
+                    if (mProgressDlg != null)
+                        mProgressDlg.dismiss();
+                }
+            };// mThread
+
+            mThread.start();
         }// onClick()
+
+        /**
+         * Generates an external temporary directory name {@code "huge-dir"}. It
+         * will be:<br>
+         * <li>{@code /mnt/sdcard/Android/data/<package_name>/cache/huge-dir}</li>
+         * <br>
+         * 
+         * @see <a href=
+         *      "http://developer.android.com/guide/topics/data/data-storage.html#filesExternal"
+         *      >Guide</a> for more information.<br>
+         * 
+         * @param context
+         *            {@link Context}
+         * @return {@link File} or {@code null} if an error occurred.
+         */
+        private File createOrRetrieveExternalTempDir(Context context) {
+            File dir = Environment.getExternalStorageDirectory();
+            if (dir == null)
+                return null;
+
+            dir = new File(String.format("%s/Android/data/%s/cache/huge-dir", dir.getAbsolutePath(),
+                    context.getApplicationInfo().packageName));
+            if (dir.isDirectory())
+                return dir;
+            return dir.mkdirs() ? dir : null;
+        }// createOrRetrieveExternalTempDir()
     };// mBtnCreateHugeDirOnClickListener
 }
