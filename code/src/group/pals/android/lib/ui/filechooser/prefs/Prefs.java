ee429f730f998b0d7cd3d7f54fe4aa5a2d1d3ddc
hai bison <none@none>
2012-10-13 05:38:26 +0700

    - Uses an unique filename of SharedPreferences instead of default application's SharedPreferences;  - Prefixes all preference keys with 'afc.';  - Updates demo;

--- code/src/group/pals/android/lib/ui/filechooser/prefs/Prefs.java
+++ code/src/group/pals/android/lib/ui/filechooser/prefs/Prefs.java
@@ -16,9 +16,9 @@
 
 package group.pals.android.lib.ui.filechooser.prefs;
 
+import group.pals.android.lib.ui.filechooser.R;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 /**
  * Convenient class for working with preferences.
@@ -29,6 +29,25 @@ import android.preference.PreferenceManager;
 public class Prefs {
 
     /**
+     * This unique ID is used for storing preferences.
+     * 
+     * @since v4.9 beta
+     */
+    public static final String _Uid = "9795e88b-2ab4-4b81-a548-409091a1e0c6";
+
+    /**
+     * Generates global preference filename of this library.
+     * 
+     * @param context
+     *            {@link Context} - will be used to obtain the application
+     *            context.
+     * @return the global preference filename.
+     */
+    public static final String genPreferenceFilename(Context context) {
+        return String.format("%s_%s", context.getString(R.string.afc_lib_name), _Uid);
+    }
+
+    /**
      * Gets new {@link SharedPreferences}
      * 
      * @param context
@@ -37,6 +56,6 @@ public class Prefs {
      */
     public static SharedPreferences p(Context context) {
         // always use application context
-        return PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return context.getApplicationContext().getSharedPreferences(genPreferenceFilename(context), 0);
     }
 }
