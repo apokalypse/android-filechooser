22c3c8368554fc704f4d2060113210b30b41f837
hai bison <none@none>
2012-12-10 20:05:16 +0700

    Fixes regexes for recognizing file types.

--- code/src/group/pals/android/lib/ui/filechooser/utils/MimeTypes.java
+++ code/src/group/pals/android/lib/ui/filechooser/utils/MimeTypes.java
@@ -19,24 +19,24 @@ public class MimeTypes {
     /**
      * Regular expression for plain text files.
      */
-    public static final String _RegexFileTypePlainTexts = "(?si).+\\.(txt|html?|json|csv|java|pas|php.*|c|cpp|"
+    public static final String _RegexFileTypePlainTexts = "(?si)^.+\\.(txt|html?|json|csv|java|pas|php.*|c|cpp|"
             + "bas|python|js|javascript|scala|xml|kml|css|ps|xslt?|tpl|tsv|bash|cmd|pl|pm|ps1|ps1xml|psc1|psd1|psm1|"
             + "py|pyc|pyo|r|rb|sdl|sh|tcl|vbs|xpl|ada|adb|ads|clj|cls|cob|cbl|cxx|cs|csproj|d|e|el|go|h|hpp|hxx|l|"
-            + "m|url|ini|prop|conf|properties|rc)";
+            + "m|url|ini|prop|conf|properties|rc)$";
 
     /**
      * Regular expression for HTML files.
      */
-    public static final String _RegexFileTypeHtmls = "(?si).+\\.(html?)";
+    public static final String _RegexFileTypeHtmls = "(?si)^.+\\.(html?)$";
 
     /**
      * Regular expression for image files.
      * 
      * @see http://en.wikipedia.org/wiki/Image_file_formats
      */
-    public static final String _RegexFileTypeImages = "(?si).+\\.(gif|jpe?g|png|tiff?|wmf|emf|jfif|exif|"
+    public static final String _RegexFileTypeImages = "(?si)^.+\\.(gif|jpe?g|png|tiff?|wmf|emf|jfif|exif|"
             + "raw|bmp|ppm|pgm|pbm|pnm|webp|riff|tga|ilbm|img|pcx|ecw|sid|cd5|fits|pgf|xcf|svg|pns|jps|icon?|"
-            + "jp2|mng|xpm|djvu)";
+            + "jp2|mng|xpm|djvu)$";
 
     /**
      * Regular expression for audio files.
@@ -44,27 +44,27 @@ public class MimeTypes {
      * @see http://en.wikipedia.org/wiki/Audio_file_format
      * @see http://en.wikipedia.org/wiki/List_of_file_formats
      */
-    public static final String _RegexFileTypeAudios = "(?si).+\\.(mp[2-3]+|wav|aiff|au|m4a|ogg|raw|flac|"
-            + "mid|amr|aac|alac|atrac|awb|m4p|mmf|mpc|ra|rm|tta|vox|wma)";
+    public static final String _RegexFileTypeAudios = "(?si)^.+\\.(mp[2-3]+|wav|aiff|au|m4a|ogg|raw|flac|"
+            + "mid|amr|aac|alac|atrac|awb|m4p|mmf|mpc|ra|rm|tta|vox|wma)$";
 
     /**
      * Regular expression for video files.
      * 
      * @see http://en.wikipedia.org/wiki/Video_file_formats
      */
-    public static final String _RegexFileTypeVideos = "(?si).+\\.(mp[4]+|flv|wmv|webm|m4v|3gp|mkv|mov|mpe?g|rmv?|ogv|"
-            + "avi)";
+    public static final String _RegexFileTypeVideos = "(?si)^.+\\.(mp[4]+|flv|wmv|webm|m4v|3gp|mkv|mov|mpe?g|rmv?|ogv|"
+            + "avi)$";
 
     /**
      * Regular expression for APK files.
      */
-    public static final String _RegexFileTypeApks = "(?si).+\\.apk";
+    public static final String _RegexFileTypeApks = "(?si)^.+\\.apk$";
 
     /**
      * Regular expression for compressed files.
      * 
      * @see http://en.wikipedia.org/wiki/List_of_file_formats
      */
-    public static final String _RegexFileTypeCompressed = "(?si).+\\.(zip|7z|lz?|[jrt]ar|gz|gzip|bzip|xz|cab|sfx|"
-            + "z|iso|bz?|rz|s7z|apk|dmg)";
+    public static final String _RegexFileTypeCompressed = "(?si)^.+\\.(zip|7z|lz?|[jrt]ar|gz|gzip|bzip|xz|cab|sfx|"
+            + "z|iso|bz?|rz|s7z|apk|dmg)$";
 }
