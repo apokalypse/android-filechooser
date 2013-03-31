5dcdb71d38b72c1dcf82a99cc47e9b0e1d100fa2
hai bison <none@none>
2013-03-31 16:17:51 +0700

    - Optimize themes for large screens and Google TV;  - Update E  ^^
    --HG--
    rename : code/res/layout-large/afc_viewgroup_history.xml => code/res/layout/afc_viewgroup_history_large.xml

--- code/src/group/pals/android/lib/ui/filechooser/utils/E.java
+++ code/src/group/pals/android/lib/ui/filechooser/utils/E.java
@@ -7,10 +7,15 @@
 
 package group.pals.android.lib.ui.filechooser.utils;
 
-import group.pals.android.lib.ui.filechooser.utils.ui.Dlg;
-import android.app.AlertDialog;
+import group.pals.android.lib.ui.filechooser.R;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 /**
  * Something funny :-)
@@ -37,11 +42,33 @@ public class E {
             msg = "Oops… You've found a broken Easter egg, try again later  :-(";
         }
 
-        AlertDialog dialog = Dlg.newDlg(context);
-        dialog.setButton(DialogInterface.BUTTON_NEGATIVE, null,
-                (DialogInterface.OnClickListener) null);
-        dialog.setTitle("…");
-        dialog.setMessage(msg);
+        final Context ctw = new ContextThemeWrapper(context,
+                R.style.Afc_Theme_Dialog_Dark);
+
+        final int padding = ctw.getResources().getDimensionPixelSize(
+                R.dimen.afc_10dp);
+        TextView textView = new TextView(ctw);
+        textView.setText(msg);
+        textView.setPadding(padding, padding, padding, padding);
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    ctw.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+                            .parse("http://www.haibison.com")));
+                } catch (Throwable t) {
+                    /*
+                     * Ignore it.
+                     */
+                }
+            }// onClick()
+        });
+
+        Dialog dialog = new Dialog(ctw, R.style.Afc_Theme_Dialog_Dark);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setContentView(textView);
         dialog.show();
     }// show()
 }
